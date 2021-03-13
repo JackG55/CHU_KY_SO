415c4336cce19e7ed18d5f4fa09fdf6df2986ae3
JackG55 <jacksparrow551999@gmail.com>
2021-03-13 07:23:56 +0700

    tao chuong trinh

---
+++ Quan_ly_VB_Chu_ky_so/Quan_ly_VB_Chu_ky_so/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Quan_ly_VB_Chu_ky_so
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Quan_ly_VB_Chu_ky_so/Quan_ly_VB_Chu_ky_so/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.UserSkins;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+
+namespace Quan_ly_VB_Chu_ky_so
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BonusSkins.Register();
+            Application.Run(new Form1());
+        }
+    }
+}
