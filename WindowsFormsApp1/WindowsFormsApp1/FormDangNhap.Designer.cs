d90d62a01715d7e3d5e4622c4d5feea4814a23de
truongan19999 <72380225+truongan19999@users.noreply.github.com>
2021-03-20 07:45:41 +0700

    form dang nhap

---
+++ WindowsFormsApp1/WindowsFormsApp1/FormDangNhap.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class FormDangNhap
+	{
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtdangnhap = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.lblpass = new System.Windows.Forms.Label();
+            this.btndangnhap = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÀI KHOẢN";
+            // 
+            // txtdangnhap
+            // 
+            this.txtdangnhap.Location = new System.Drawing.Point(196, 161);
+            this.txtdangnhap.Name = "txtdangnhap";
+            this.txtdangnhap.Size = new System.Drawing.Size(199, 21);
+            this.txtdangnhap.TabIndex = 1;
+            this.txtdangnhap.Text = "121";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(196, 194);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(199, 21);
+            this.txtpass.TabIndex = 3;
+            this.txtpass.Text = "1212";
+            // 
+            // lblpass
+            // 
+            this.lblpass.AutoSize = true;
+            this.lblpass.Location = new System.Drawing.Point(105, 197);
+            this.lblpass.Name = "lblpass";
+            this.lblpass.Size = new System.Drawing.Size(58, 13);
+            this.lblpass.TabIndex = 2;
+            this.lblpass.Text = "MẬT KHẨU";
+            // 
+            // btndangnhap
+            // 
+            this.btndangnhap.Location = new System.Drawing.Point(166, 242);
+            this.btndangnhap.Name = "btndangnhap";
+            this.btndangnhap.Size = new System.Drawing.Size(75, 23);
+            this.btndangnhap.TabIndex = 4;
+            this.btndangnhap.Text = "Đăng nhập";
+            this.btndangnhap.UseVisualStyleBackColor = true;
+            this.btndangnhap.Click += new System.EventHandler(this.btndangnhap_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.hoc_vien_ki_thuat_quan_su_he_quan_su1;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(77, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(191, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 58);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "HỆ THỐNG QUẢN LÝ\r\nVĂN BẢN ĐIỆN TỬ";
+            // 
+            // FormDangNhap
+            // 
+            this.AcceptButton = this.btndangnhap;
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 305);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btndangnhap);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.lblpass);
+            this.Controls.Add(this.txtdangnhap);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDangNhap";
+            this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtdangnhap;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label lblpass;
+        private System.Windows.Forms.Button btndangnhap;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
