e34163b657456969f0290ef0eb6024b832d01c7d
JackG55 <jacksparrow551999@gmail.com>
2021-04-09 23:16:15 +0700

    Update connection.cs

--- WindowsFormsApp1/WindowsFormsApp1/Connect/connection.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Connect/connection.cs
@@ -1,13 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp1.Connect
 {
     public class connection
     {
         public static string connectionString = "Data Source=DESKTOP-MSHI2VU;Initial Catalog=Quan_ly_van_ban;Integrated Security=True";
+
+        public static SqlConnection Getconnection()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            return con;
+        }
+
+        public static void open()
+        {
+            try
+            {
+                if (Getconnection().State == ConnectionState.Closed)
+                    Getconnection().Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message);
+            }
+        }
+        public static void close()
+        {
+            try
+            {
+                if (Getconnection().State == ConnectionState.Open)
+                    Getconnection().Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
 }
