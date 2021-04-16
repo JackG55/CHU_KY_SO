caf07e1d2ccadee59d6350bdbd32384ca4339c5e
JackG55 <jacksparrow551999@gmail.com>
2021-04-16 14:53:32 +0700

    hi

---
+++ WindowsFormsApp1/WindowsFormsApp1/Chu_ky_so/Quan_ly_chu_ky.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace WindowsFormsApp1.Chu_ky_so
+{
+    public partial class Quan_ly_chu_ky : DevExpress.XtraEditors.XtraForm
+    {
+        public Quan_ly_chu_ky()
+        {
+            InitializeComponent();
+        }
+    }
+}
